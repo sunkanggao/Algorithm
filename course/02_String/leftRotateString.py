058cdc72e3fda97b4190c682b7c35030283a1f8b
sunkanggao <sunkanggao1992@gmail.com>
2017-09-05 19:34:00 +0800

    2017/09/05

---
+++ course/02_String/leftRotateString.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+
+"""
+字符串循环左移
+"""
+
+def reverseString(s, i, j):
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+
+
+def leftRotateString(s, n, m):
+    """
+    :param s: 字符串
+    :param n: 字符串长度
+    :param m: 循环左移位数
+    :return:
+    """
+    m %= n
+    reverseString(s, 0, m - 1)
+    reverseString(s, m, n - 1)
+    reverseString(s, 0, n - 1)
+    return  s
+
+if __name__ == '__main__':
+    s = 'abcdef'
+    print s
+    print ''.join(leftRotateString(list(s), len(s), 2))
