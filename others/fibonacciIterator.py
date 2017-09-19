f542be8019434d6e45cfee694d36ed41a02fe021
sunkanggao <sunkanggao1992@gmail.com>
2017-09-19 13:47:38 +0800

    二分查找

--- others/fibonacciIterator.py
+++ others/fibonacciIterator.py
@@ -7,23 +7,21 @@ __metaclass__ = type
 class Fibs:
     def __init__(self, max):
         self.max = max
-        self.n = 0
-        self.a = 1
-        self.b = 1
+        self.n, self.a, self.b = 0, 0, 1
 
     def __iter__(self):
         return self
 
     def next(self):
-        fibs = self.a
-        self.n += 1
-        if self.n > self.max:
-            raise StopIteration
-        self.a, self.b = self.b, self.a + self.b
-        return fibs
+        if self.n < self.max:
+            self.a, self.b = self.b, self.a + self.b
+            self.n += 1
+            return self.a
+        raise StopIteration()
+
 
 if __name__ == '__main__':
     t = time.time()
-    fibs = Fibs(30)
-    print list(fibs),
+    for i in Fibs(5):
+        print i
     print t - time.time()
